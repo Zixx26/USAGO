659d044b7d4da981f55bab253008e6c64a360eb1
Zixx26 <ostrovskyidanyyl@gmail.com>
2025-04-21 16:58:41 +0300

    finally fixed the problem

--- Broken_Necklace.py
+++ Broken_Necklace.py
@@ -3,12 +3,13 @@ ID: lel
 LANG: PYTHON3
 PROG: beads
 """
-fin = open ('beads.in', 'r')
+fin = open('beads.in', 'r')
 fout = open ('beads.out', 'w')
 fin.readline()
 halfNecklace = fin.readline().replace("\n","")
-if  len(halfNecklace)%3 == 0:fullNecklace = halfNecklace
+if halfNecklace[0:2] == halfNecklace[-2:] or len(halfNecklace)%4 == 0:fullNecklace = halfNecklace
 else:fullNecklace = halfNecklace + halfNecklace
+
 
 
 fullNecklace = fullNecklace.replace("\n","")
@@ -21,14 +22,33 @@ stage = " "
 
 current = 0
 
-Cwhites = [0,0]
-CwhitesCounter = [0,0]
+Cwhites = [0,0,0]
+CwhitesCounter = [0,0,0]
 
 CCvalue = [0,0]
 BiggestValue = 0
 shifts=2
 
+def whiteshift():
+    for j in range(len(CwhitesCounter)):
+        
+        if CwhitesCounter[j] == 0:
+            Cwhites[j] = 0
+        else:CwhitesCounter[j]-=1
 
+def whitechange():
+    if Cwhites[0]==0: 
+        Cwhites[0]=white
+        CwhitesCounter[0]=shifts
+    elif Cwhites[1]==0:
+        Cwhites[1]=white
+        CwhitesCounter[1]=shifts
+    else:
+        Cwhites[2]=white
+        CwhitesCounter[2]=shifts
+        
+
+       
 
 for i in fullNecklace:
     
@@ -51,10 +71,7 @@ for i in fullNecklace:
 
             
 
-            for j in range(len(CwhitesCounter)-1):
-                if CwhitesCounter[j] == 0:
-                  Cwhites[j] = 0
-                else:CwhitesCounter[j]-=1
+            whiteshift()
                    
                                
             
@@ -62,39 +79,29 @@ for i in fullNecklace:
 
             if white != 0:
                 things.append(str(white))
-                if Cwhites[0]==0: 
-                    Cwhites[0]=white
-                    CwhitesCounter[0]=shifts
-                else:
-                    Cwhites[1]=white
-                    CwhitesCounter[1]=shifts
+                whitechange()
                 white=0
            
-            if Cwhites[0]+Cwhites[1]+CCvalue[0]+CCvalue[1]> BiggestValue:BiggestValue= Cwhites[0]+Cwhites[1]+CCvalue[0]+CCvalue[1]
             
-for j in range(len(CwhitesCounter)-1):
-    if CwhitesCounter[j] == 0:
-        Cwhites[j] = 0
-    else:CwhitesCounter[j]-=1
+
+            if Cwhites[0]+Cwhites[1]+Cwhites[2]+CCvalue[0]+CCvalue[1]> BiggestValue:
+                BiggestValue = Cwhites[0]+Cwhites[1]+Cwhites[2]+CCvalue[0]+CCvalue[1]
+                
+
+
+whiteshift()
+
+whitechange()
 
 CCvalue[0],CCvalue[1] = CCvalue[1],current
-if Cwhites[0]==0: 
-    Cwhites[0]=white
-    CwhitesCounter[0]=shifts
-else:
-    Cwhites[1]=white
-    CwhitesCounter[1]=shifts
 
-if Cwhites[0]+Cwhites[1]+CCvalue[0]+CCvalue[1]> BiggestValue:BiggestValue= Cwhites[0]+Cwhites[1]+CCvalue[0]+CCvalue[1]
+if Cwhites[0]+Cwhites[1]+Cwhites[2]+CCvalue[0]+CCvalue[1]> BiggestValue: 
+    BiggestValue = Cwhites[0]+Cwhites[1]+Cwhites[2]+CCvalue[0]+CCvalue[1]
+    print(CCvalue,Cwhites,things)
 
 fout.write(str(BiggestValue) + "\n")   
     
-print(Cwhites[0],Cwhites[1],CCvalue[0],CCvalue[1])
 
-"74"
-#74 is the answer to
-#77
-#rwrwrwrwrwrwrwrwrwrwrwrwbwrwbwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwr
 
 
 
